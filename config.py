c6b134149e7f52a51b114037cd30c719c33b9689
Orewa0Luffy <ichigobotam1@gmail.com>
2025-06-15 10:14:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
+# config.py
+
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", 123456))
+API_HASH = os.getenv("API_HASH", "your_api_hash")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+MONGO_URL = os.getenv("MONGO_URL", "your_mongo_url")
+
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100xxxxxxxxxx"))
+OWNER_ID = int(os.getenv("OWNER_ID", 123456789))
